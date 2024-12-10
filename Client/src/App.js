8c6632178773f968c65fb4b9be670425e1de949a
Mohamed Darwesh <midodarwesh285@gmail.com>
2024-12-10 21:33:51 +0200

    add product mapping

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router';
 import Cart from './components/Cart/Cart';
 import Home from './components/Home/Home';
+import data from './Data.json'
 
 function App() {
   return <>
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home products={data} />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
   </>
